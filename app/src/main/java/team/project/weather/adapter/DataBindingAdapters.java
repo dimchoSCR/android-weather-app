49d9133c667634ae7875fed6453c13a1a8c4a864
dimchoSCR <dimchokarpachev@gmail.com>
2016-04-19 13:32:12 +0300

    Added custom data binding attributes

---
+++ app/src/main/java/team/project/weather/adapter/DataBindingAdapters.java
@@ -0,0 +1,25 @@
+package team.project.weather.adapter;
+
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DataBindingAdapters {
+    static SimpleDateFormat sdf = new SimpleDateFormat("dd.MM HH:mm", Locale.US);
+
+    @BindingAdapter("weather:src")
+    public static void setImageResource(ImageView imageView, int resource){
+        imageView.setImageResource(resource);
+    }
+
+    // Formats the date to display in the TextView via the new :date attribute
+    @BindingAdapter("weather:date")
+    public static void setDate(TextView textView, Date date){
+        String formattedDate = "Last updated: " + sdf.format(date);
+        textView.setText(formattedDate);
+    }
+}
