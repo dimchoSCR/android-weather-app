009ffb1d5790276e951db554bbbf462f721af657
dimchoSCR <dimchokarpachev@gmail.com>
2016-04-19 13:53:07 +0300

    Added getters and setters

--- app/src/main/java/team/project/weather/model/Model.java
+++ app/src/main/java/team/project/weather/model/Model.java
@@ -1,10 +1,21 @@
 package team.project.weather.model;
 
+import android.databinding.ObservableField;
 
 import team.project.weather.model.Day;
 
 public class Model {
-    Day currentDay;
+    private Day currentDay;
 
+    public Model(Day day){
+        currentDay = day;
+    }
 
+    public Day getCurrentDay() {
+        return currentDay;
+    }
+
+    public void setCurrentDay(Day currentDay) {
+        this.currentDay = currentDay;
+    }
 }
