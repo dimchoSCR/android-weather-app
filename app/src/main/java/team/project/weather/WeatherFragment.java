e4e9cfaac7a73707e87b1789bbf40b3856807a1d
dimchoSCR <dimchokarpachev@gmail.com>
2016-04-23 17:08:30 +0300

    Created the Updater thread. Integrated the CacheManager and the OpenWeatherService classes

--- app/src/main/java/team/project/weather/WeatherFragment.java
+++ app/src/main/java/team/project/weather/WeatherFragment.java
@@ -3,13 +3,15 @@ package team.project.weather;
 import android.app.Fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import team.project.weather.databinding.FragmentMainBinding;
 import team.project.weather.model.Day;
@@ -17,22 +19,70 @@ import team.project.weather.model.Model;
 
 public class WeatherFragment extends Fragment {
 
+    private static final int UPDATE_INTERVAL_HOURS = 2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        FragmentMainBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false);
+        FragmentMainBinding binding = DataBindingUtil.inflate(
+                inflater,R.layout.fragment_main,container,false);
 
         View weatherView = binding.getRoot();
 
-        Day day = new Day();
-        day.setLocationCity("Sofia");
-        day.setTemperature(40f);
-        day.setWeather(Day.Weather.CLOUDY);
-        day.setWindSpeed(12);
-        day.setLastUpdated(new Date());
+        Day day;
+        Model model;
+        CacheManager manager = new CacheManager();
 
-        Model model = new Model(day);
+        // Retrieve data from cache if possible
+        try{
+            day = manager.retrieve();
+        }catch(Exception err){
+            Log.e("Cache","Error retrieving data from cache",err);
+            Toast.makeText(weatherView.getContext(),
+                    "Unable to load stored weather data. Please wait for an update from the network"
+                    ,Toast.LENGTH_LONG).show();
+            day = new Day();
+        }
+
+        model = new Model();
+        model.currentDay.set(day);
         binding.setModel(model);
 
+        // Setting up a scheduled Executor inorder to execute the thread
+        // within the UPDATE_INTERVAL_HOURS
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new UpdaterThread(model),0, UPDATE_INTERVAL_HOURS, TimeUnit.HOURS);
+
         return weatherView;
+    }
+
+    private class UpdaterThread implements Runnable{
+        private Model model;
+        private OpenWeatherService openWeatherService;
+
+        UpdaterThread(Model model){
+            this.model = model;
+            openWeatherService = new OpenWeatherService();
+        }
+
+        @Override
+        public void run(){
+            //TODO Get GPS coordinates
+            try {
+                // No need to synchronize with the UI thread because of the data binding
+                model.currentDay.set(openWeatherService.getCurrentDay(0,0));
+            } catch (Exception err) {
+                Log.e("WeatherUpdate","Could not update weather!",err);
+                // Post the toast on the UI thread's MessageQueue
+                MainActivity.toastHandler.post(new Runnable(){
+                    @Override
+                    public void run(){
+                        Toast.makeText(getActivity().getApplicationContext(),
+                                "Filed to update weather! Please check your internet connection.",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+        }
     }
 }
