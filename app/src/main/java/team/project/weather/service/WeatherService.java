9b9e532d157e34d717cf44fd74825c86b7fb165a
dimchoSCR <dimchokarpachev@gmail.com>
2016-05-03 22:22:05 +0300

    Changed the type of the parameters

--- app/src/main/java/team/project/weather/service/WeatherService.java
+++ app/src/main/java/team/project/weather/service/WeatherService.java
@@ -3,5 +3,5 @@ package team.project.weather.service;
 import team.project.weather.model.Day;
 
 public interface WeatherService {
-    Day getCurrentDay(float lat,float lon) throws Exception;
+    Day getCurrentDay(double lat,double lon) throws Exception;
 }
